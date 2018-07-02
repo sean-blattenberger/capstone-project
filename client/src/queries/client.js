3813067fc5ba028ef659a56d37e1120d37138786
sean-blattenberger <sean.blattenberger@gmail.com>
2018-07-02 16:12:25 -0600

    Added herorku api url

--- client/src/queries/client.js
+++ client/src/queries/client.js
@@ -2,7 +2,7 @@ import { Provider, Client } from 'urql';
 
 
 export const client = new Client({
-  url: 'http://localhost:4000/graphql'
+  url: 'https://menu-sort.herokuapp.com/graphql'
 })
 
 export {Provider};
